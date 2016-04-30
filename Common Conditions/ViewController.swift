29c90809b5916ce05833fca56a96cd664933fd0b
cambardell <cambardell3@gmail.com>
2016-04-29 23:02:46 -0400

    Initial Commit

---
+++ Common Conditions/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Common Conditions
+//
+//  Created by Cameron Bardell on 2016-04-29.
+//  Copyright © 2016 Cameron Bardell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
